2ae4103ff55a34583c3046e3424713a91011f255
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-03-23 21:17:50 +0900

    キャリブパラメタを入力

--- src/remap_node.cpp
+++ src/remap_node.cpp
@@ -17,29 +17,30 @@ cv::Mat rmapx,rmapy;
 
 bool reload(std_srvs::Trigger::Request &req,std_srvs::Trigger::Response &res){
 	res.success=false;
+	res.message="";
 	std::vector<double> D;
    nh->getParam("remap/D",D);
-	if(D.size()<5){
-		res.message="D NG";
-		return true;
+	if(D.size()!=5){
+		ROS_ERROR("Param D NG");
+		res.message+="D NG/";
 	}
 	std::vector<double> K;
 	nh->getParam("remap/K",K);
-	if(K.size()<9){
-		res.message="K NG";
-		return true;
+	if(K.size()!=9){
+		ROS_ERROR("Param K NG");
+		res.message+="K NG/";
 	}
 	std::vector<double> R;
 	nh->getParam("remap/R",R);
-	if(R.size()<9){
-		res.message="R NG";
-		return true;
+	if(R.size()!=9){
+		ROS_ERROR("Param R NG");
+		res.message+="R NG/";
 	}
 	std::vector<double> P;
 	nh->getParam("remap/P",P);
-	if(P.size()<12){
-		res.message="P NG";
-		return true;
+	if(P.size()!=12){
+		ROS_ERROR("Param P NG");
+		res.message+="P NG/";
 	}
 	cv::Mat Pro(P),nCam,nRot,nTrans;
 	cv::OutputArray oCam(nCam),oRot(nRot),oTrans(nTrans);
@@ -49,9 +50,9 @@ bool reload(std_srvs::Trigger::Request &req,std_srvs::Trigger::Response &res){
 	nh->getParam("remap/height",height);
 	cv::Size imgsz(width,height);
 	if(imgsz.area()==0){
-		res.message="Size NG";
-		return true;
+		res.message+="Size NG/";
 	}
+	if(res.message.size()>0) return true;
 	cv::Mat Cam(K),Rot(R);
 	cv::initUndistortRectifyMap(Cam.reshape(1,3),D,Rot.reshape(1,3),nCam,imgsz,CV_32FC1,rmapx,rmapy);
 	res.success=true;
@@ -87,6 +88,7 @@ int main(int argc, char **argv){
 	std_srvs::Trigger::Request req;
 	std_srvs::Trigger::Response res;
 	reload(req,res);
-	ros::spin();
+	if(res.success) ros::spin();
+	else ROS_ERROR("remap:unmatched parameters");
 	return 0;
 }
