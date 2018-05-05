89a158439b1a35f992c1fe65c16633da722b4e0e
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-05-05 15:45:36 +0900

    grid_nodeにキャリブ板Pose出力を追加

--- src/grid_node.cpp
+++ src/grid_node.cpp
@@ -1,56 +1,83 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
 #include <sensor_msgs/Image.h>
 #include <std_srvs/Trigger.h>
 #include "rovi/GetGrid.h"
 #include <vector>
 #include <string>
+#include <cstdio>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include "CircleCalibBoard.h"
+#include <tf/transform_datatypes.h>
 
 //ros::Publisher pub;
 
 CircleCalibBoard cboard;
+ros::NodeHandle *nh;
 
 bool get_grid(rovi::GetGrid::Request &req,rovi::GetGrid::Response &res){
-//	ROS_INFO("get_grids notif %d",req.notif.size());
-	cv_bridge::CvImagePtr cv_ptr2,cv_ptr1;
+	cv_bridge::CvImagePtr cv_ptr1;
 	try{
 		cv_ptr1=cv_bridge::toCvCopy(req.img,sensor_msgs::image_encodings::MONO8);
-//		cv_ptr2=cv_bridge::toCvCopy(req.img,sensor_msgs::image_encodings::BGR8);
+//		cv_ptr1=cv_bridge::toCvCopy(req.img,sensor_msgs::image_encodings::BGR8);
 	}
-	catch (cv_bridge::Exception& e){
+	catch(cv_bridge::Exception& e){
 		ROS_ERROR("get_grids:cv_bridge:exception: %s", e.what());
 		return false;
 	}
 	cboard.init();
 	std::vector<cv::Point2f> imagePoints;
-//	imagePoints.resize(cboard.para["n_circles_x"] * cboard.para["n_circles_y"]);
 	cv::Mat mat;
 	if(cboard.scan(cv_ptr1->image,imagePoints,&mat)){
-//	if(cboard.scan(cv_ptr1->image,imagePoints,&cv_ptr2->image)){
 		ROS_ERROR("CircleCalibBoard::scan:failed:");
 	}
 	else{
+		int nx=cboard.para["n_circles_x"];
+		int ny=cboard.para["n_circles_y"];
+		std::vector<cv::Point3f> model;
+		std::vector<cv::Point2f> scene;
 		geometry_msgs::Point p;
 		p.z=0;
 		for(int i=0;i<imagePoints.size();i++){
-			p.x=imagePoints[0].x;
-			p.y=imagePoints[0].y;
+			if(imagePoints[i].x==FLT_MAX || imagePoints[i].y==FLT_MAX) continue;
+			p.x=imagePoints[i].x;
+			p.y=imagePoints[i].y;
 			res.grid.push_back(p);
+			cv::Point3f pm(cboard.get_3d_position(i));
+			cv::Point2f ps(p.x,p.y);
+			model.push_back(pm);
+			scene.push_back(ps);
+//std::cout<<"model::"<<pm;
+//std::cout<<" scene::"<<ps<<"\n";
 		}
+		double flen=req.img.width/2; //Approx. focal length
+		nh->getParam("gridboard/focalLength",flen);
+		std::cout<<" focal::"<<flen<<"\n";
+		cv::Point2f cen=cv::Point2f(req.img.width/2,req.img.height/2);  //Center of image
+		cv::Mat kmat=(cv::Mat_<float>(3,3)<<flen,0,cen.x,0,flen,cen.y,0,0,1);
+		cv::Mat dmat=cv::Mat::zeros(4,1,cv::DataType<float>::type); //No distortion
+		cv::Mat rmat,tmat;
+		cv::OutputArray oRmat(rmat),oTmat(tmat);
+		cv::solvePnP(model,scene,kmat,dmat,oRmat,oTmat);
+		std::cout<<"R "<<rmat<<"\n";
+		tf::Quaternion qt=tf::createQuaternionFromRPY(rmat.data[0],rmat.data[1],rmat.data[2]);
+		geometry_msgs::Quaternion qm;
+		quaternionTFToMsg(qt,qm);
+		std::cout<<"Q ["<<qm.x<<","<<qm.y<<","<<qm.z<<","<<qm.w<<"]\n";
+		res.pose.position.x=tmat.data[0];
+		res.pose.position.y=tmat.data[1];
+		res.pose.position.z=tmat.data[2];
+		res.pose.orientation=qm;
 	}
 	cv_ptr1->image=mat;
 	cv_ptr1->encoding="bgr8";
 	cv_ptr1->toImageMsg(res.img);
-//	cv_ptr2->toImageMsg(res.img);
-//	pub.publish(cv_ptr->toImageMsg());
 	return true;
 }
 
-ros::NodeHandle *nh;
 bool reload(std_srvs::Trigger::Request &req,std_srvs::Trigger::Response &res){
 	for(std::map<std::string, double>::iterator itr=cboard.para.begin();itr!=cboard.para.end();++itr){
 		std::string pname("gridboard/");
