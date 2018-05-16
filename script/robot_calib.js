125824de5d062ad5acd9bc0b2875f2a67b21da7e
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-05-16 22:11:28 +0900

    佐藤さん結果と比較中

--- script/robot_calib.js
+++ script/robot_calib.js
@@ -6,33 +6,73 @@ const geometry_msgs=ros.require('geometry_msgs').msg;
 const std_msgs=ros.require('std_msgs').msg;
 const std_srvs=ros.require('std_srvs').srv;
 const rovi_srvs=ros.require('rovi').srv;
+const visp_msgs=ros.require('visp_hand2eye_calibration').msg;
+const visp_srvs=ros.require('visp_hand2eye_calibration').srv;
 const EventEmitter=require('events').EventEmitter;
+
 
 setImmediate(async function(){
 	const rosNode=await ros.initNode('robot_calib');
 	const evs=new EventEmitter();
-	let pos_grid=new geometry_msgs.Pose();
-	let cl_grid=rosNode.serviceClient('/gridboard',rovi_srvs.GetGrid,{persist:true});
+	const cl_grid=rosNode.serviceClient('/gridboard',rovi_srvs.GetGrid,{persist:true});
 	if(! rosNode.waitForService(cl_grid.getService(),1000)){
 		ros.log.error('grid_node not available');
 		return;
 	}
+	const cl_calib=rosNode.serviceClient('/compute_effector_camera_quick',visp_srvs.compute_effector_camera_quick,{persist:true});
+	if(! rosNode.waitForService(cl_calib.getService(),1000)){
+		ros.log.error('calibrator::compute not available');
+		return;
+	}
+	const cl_reset=rosNode.serviceClient('/reset',visp_srvs.reset,{persist:true});
+	if(! rosNode.waitForService(cl_reset.getService(),1000)){
+		ros.log.error('calibrator::reset not available');
+		return;
+	}
+	let c2o=new visp_msgs.TransformArray();
+	let w2e=new visp_msgs.TransformArray();
 	const svc1=rosNode.advertiseService('/robot_calib/pose',rovi_srvs.SetPose,(req,res)=>{
-		ros.log.info('robot_pose:'+JSON.stringify(req.pose));
+//		ros.log.info('robot_pose:'+JSON.stringify(req.pose));
+		let trm=new geometry_msgs.Transform();
+		trm.translation=req.pose.position;
+		trm.rotation=req.pose.orientation;
 		return new Promise(function(resolve){
-			evs.once('pose',function(pose){
+			evs.once('pose',async function(pose){
 				ros.log.info('object_pose:'+JSON.stringify(pose));
+				let tro=new geometry_msgs.Transform();
+				let m2mm=await rosNode.getParam('/gridboard/unitmm');
+				let m2px=await rosNode.getParam('/gridboard/unitleng');
+				let px2mm=m2mm/m2px;
+				tro.translation.x=pose.position.x*px2mm;
+				tro.translation.y=pose.position.y*px2mm;
+				tro.translation.z=pose.position.z*px2mm;
+				tro.rotation=pose.orientation;
+				c2o.transforms.push(tro);
+				w2e.transforms.push(trm);
 				resolve(true);
-//				evs.removeAllListeners('pose');
 			});
 		});
 	});
-	let pub_grid=rosNode.advertise('/robot_calib/image', sensor_msgs.Image);
+	let pub_grid=rosNode.advertise('/robot_calib/image',sensor_msgs.Image);
 	let sub=rosNode.subscribe('/left/image_rect',sensor_msgs.Image,async (src)=>{
 		let req=new rovi_srvs.GetGrid.Request();
 		req.img=src;
 		let res=await cl_grid.call(req);
 		pub_grid.publish(res.img);
+
 		evs.emit('pose',res.pose);
+	});
+	const svc2=rosNode.advertiseService('/robot_calib/reload',std_srvs.Trigger,async (req,res)=>{
+		let rq=new visp_srvs.reset.Request();
+		await cl_reset.call(rq);
+		rq=new visp_srvs.compute_effector_camera_quick.Request();
+		rq.camera_object=c2o;
+		rq.world_effector=w2e;
+		let rs=await cl_calib.call(rq);
+		res.message=JSON.stringify(rs.effector_camera);
+//console.log('RES '+JSON.stringify(rs.effector_camera));
+		c2o=new visp_msgs.TransformArray();
+		w2e=new visp_msgs.TransformArray();
+		return true;
 	});
 });
---
+++ test/robot_calib.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Test program of Robot Calibraion
+#
+DataDir=/home/ca/robotcalib
+
+rosparam set /gridboard/focalLength 1800
+rosparam set /gridboard/unitleng 60.0
+rosparam set /gridboard/unitmm 5.0
+
+if ! rosnode list | grep /grid_node
+then
+	rosrun rovi grid_node &
+fi
+
+if ! rosnode list | grep /tools_node
+then
+	rosrun rovi tools_node &
+fi
+
+if ! rosnode list | grep /robot_calib
+then
+	rosrun rovi robot_calib.js &
+fi
+
+if ! rosnode list | grep /imsep
+then
+	rosrun rovi imsep.js /tools/image &
+fi
+
+if ! rosnode list | grep /xyz2q
+then
+	rosrun rovi xyz2q.js /robot_calib/pose &
+fi
+
+cd $DataDir
+rosservice call /tools/cd $DataDir
+cat robot_xyzabc.txt | for src in calib[01][0-9].pgm
+do
+	echo "---reading "$src "---"
+	read line
+	echo $line
+	rosservice call /xyz2q "'""$line""'" &
+	sleep 1
+	rosservice call /tools/imread $src
+done
+
+rosservice call /robot_calib/reload
+
