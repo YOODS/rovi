260db0ea994a414c157b4d8cebdc30521c4f9e59
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-07-24 08:09:44 +0900

    サービスlogger[0-3]追加

--- src/tools_node.cpp
+++ src/tools_node.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <std_msgs/UInt8.h>
+#include <std_srvs/SetBool.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/PointCloud.h>
 #include <opencv2/opencv.hpp>
@@ -11,6 +13,7 @@
 ros::NodeHandle *nh;
 ros::Publisher pub1;
 ros::Publisher pub2;
+ros::Publisher pub3[4];
 
 bool change_dir(rovi::Dialog::Request &req, rovi::Dialog::Response &res)
 {
@@ -67,6 +70,31 @@ bool load_ply(rovi::Dialog::Request &req, rovi::Dialog::Response &res)
   return true;
 }
 
+bool logger0(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res){
+  std_msgs::UInt8 msg;
+  msg.data=req.data;
+  pub3[0].publish(msg);
+  return res.success=true;
+}
+bool logger1(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res){
+  std_msgs::UInt8 msg;
+  msg.data=req.data;
+  pub3[1].publish(msg);
+  return res.success=true;
+}
+bool logger2(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res){
+  std_msgs::UInt8 msg;
+  msg.data=req.data;
+  pub3[2].publish(msg);
+  return res.success=true;
+}
+bool logger3(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res){
+  std_msgs::UInt8 msg;
+  msg.data=req.data;
+  pub3[4].publish(msg);
+  return res.success=true;
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "tools_node");
@@ -74,9 +102,18 @@ int main(int argc, char **argv)
   nh = &n;
   pub1 = n.advertise<sensor_msgs::Image>("tools/image", 1);
   pub2 = n.advertise<sensor_msgs::PointCloud>("tools/pcl", 1);
+  for(int i=0;i<4;i++){
+    char s[20];
+    sprintf(s,"tools/log%d",i);
+    pub3[i] = n.advertise<std_msgs::UInt8>(s, 1);
+  }
   ros::ServiceServer svc0 = n.advertiseService("tools/cd", change_dir);
   ros::ServiceServer svc1 = n.advertiseService("tools/imread", load_img);
   ros::ServiceServer svc2 = n.advertiseService("tools/plread", load_ply);
+  ros::ServiceServer svc30 = n.advertiseService("tools/logger0", logger0);
+  ros::ServiceServer svc31 = n.advertiseService("tools/logger1", logger1);
+  ros::ServiceServer svc32 = n.advertiseService("tools/logger2", logger2);
+  ros::ServiceServer svc33 = n.advertiseService("tools/logger3", logger3);
   ros::spin();
   return 0;
 }
