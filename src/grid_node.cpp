b3c25b115ba2a0d1b6b84f14f3936a06fa3e59ce
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-08-09 19:15:44 +0900

    R-Calibドキュメント

--- src/grid_node.cpp
+++ src/grid_node.cpp
@@ -2,110 +2,80 @@
 #include <std_srvs/Trigger.h>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include "rovi/GetGrid.h"
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include "rovi/ImageFilter.h"
 #include "CircleCalibBoard.h"
 
 CircleCalibBoard cboard;
 ros::NodeHandle *nh;
 std::string paramK("gridboard/K");
-static double px2mm = 0.1;
 static std::vector<double> kvec;
+static ros::Publisher *pub1, *pub2;
 
-bool get_grid(rovi::GetGrid::Request &req, rovi::GetGrid::Response &res)
-{
+bool get_grid(rovi::ImageFilter::Request &req, rovi::ImageFilter::Response &res){
   cv_bridge::CvImagePtr cv_ptr1;
-  try
-  {
+  try{
     cv_ptr1 = cv_bridge::toCvCopy(req.img, sensor_msgs::image_encodings::MONO8);
   }
-  catch(cv_bridge::Exception& e)
-  {
+  catch(cv_bridge::Exception& e){
     ROS_ERROR("get_grids:cv_bridge:exception: %s", e.what());
     return false;
   }
   std::vector<cv::Point2f> imagePoints;
   cv::Mat mat;
-  if (cboard.scan(cv_ptr1->image, imagePoints, &mat))
-  {
-    ROS_ERROR("CircleCalibBoard::scan:failed:");
-    return false;
-  }
-  else
-  {
-    cv_ptr1->image = mat;
-    cv_ptr1->encoding = "bgr8";
-    cv_ptr1->toImageMsg(res.img);
-    std::vector<cv::Point3f> model;
-    std::vector<cv::Point2f> scene;
-    geometry_msgs::Point p;
-    p.z = 0;
-    for (int i = 0; i < imagePoints.size(); i++)
-    {
-      if (imagePoints[i].x == FLT_MAX || imagePoints[i].y == FLT_MAX)
-      {
-        p.z = -1; // recognition failed
-      }
-      else
-      {
-        p.z = 0;
-      }
-      p.x = imagePoints[i].x;
-      p.y = imagePoints[i].y;
-      res.grid.push_back(p);
-      if (p.z < 0)
-      {
-        continue;
-      }
-      cv::Point3f pm(cboard.get_3d_position(i));
-      pm *= px2mm;
-      cv::Point2f ps(p.x, p.y);
-      model.push_back(pm);
-      scene.push_back(ps);
-    }
-
-/*
-    // Test of simple case
-    int ncx = (int)cboard.para["n_circles_x"];
-    int ncy = (int)cboard.para["n_circles_y"];
-    int ind00 = ncx / 2 + ncx * (ncy / 2);
-    int ind01 = ind00 + 5;
-    int ind10 = ind00 + 5 * ncx;
-    int ind11 = ind00 + 5 + 5 * ncx;
-    std::cout << "Model\n";
-    std::cout << model[ind00] << "\n" << model[ind10] << "\n" << model[ind01] << "\n" << model[ind11] << "\n";
-    std::cout << "Scene\n";
-    std::cout << scene[ind00] << "\n" << scene[ind10] << "\n" <<scene[ind01] << "\n" << scene[ind11] << "\n";
-    std::vector<cv::Point3f> mod;
-    std::vector<cv::Point2f> sce;
-    mod.push_back(model[ind00]);
-    mod.push_back(model[ind10]);
-    mod.push_back(model[ind01]);
-    mod.push_back(model[ind11]);
-    sce.push_back(scene[ind00]);
-    sce.push_back(scene[ind10]);
-    sce.push_back(scene[ind01]);
-    sce.push_back(scene[ind11]);
-*/
-
-    cv::Mat kmat(kvec);
-    cv::Mat dmat = cv::Mat::zeros(5, 1, cv::DataType<float>::type); // No distortion
-    cv::Mat rmat(3, 1, cv::DataType<double>::type);
-    cv::Mat tmat(3, 1, cv::DataType<double>::type);
-    cv::OutputArray oRmat(rmat), oTmat(tmat);
-    cv::solvePnP(model, scene, kmat.reshape(1, 3), dmat, oRmat, oTmat);
-    float rx = rmat.at<double>(0, 0);
-    float ry = rmat.at<double>(1, 0);
-    float rz = rmat.at<double>(2, 0);
-    float rw = sqrt(rx * rx + ry * ry + rz * rz);
-    res.pose.position.x = tmat.at<double>(0, 0);
-    res.pose.position.y = tmat.at<double>(1, 0);
-    res.pose.position.z = tmat.at<double>(2, 0);
-    res.pose.orientation.x = rw > 0 ? sin(rw / 2) * rx / rw : rx;
-    res.pose.orientation.y = rw > 0 ? sin(rw / 2) * ry / rw : ry;
-    res.pose.orientation.z = rw > 0 ? sin(rw / 2) * rz / rw : rz;
-    res.pose.orientation.w = cos(rw / 2);
+  int cbres=cboard.scan(cv_ptr1->image, imagePoints, &mat);
+  sensor_msgs::Image img;
+  cv_ptr1->image=mat;
+  cv_ptr1->encoding="bgr8";
+  cv_ptr1->toImageMsg(img);
+  pub1->publish(img);
+  if(cbres){
+    ROS_WARN("CircleCalibBoard::scan:failed:");
     return true;
   }
+  std::vector<cv::Point3f> model;
+  std::vector<cv::Point2f> scene;
+  geometry_msgs::Point p;
+  p.z = 0;
+  for (int i = 0; i < imagePoints.size(); i++){
+    if (imagePoints[i].x == FLT_MAX || imagePoints[i].y == FLT_MAX){
+      p.z = -1; // recognition failed
+    }
+    else{
+      p.z = 0;
+    }
+    p.x = imagePoints[i].x;
+    p.y = imagePoints[i].y;
+    if (p.z < 0){
+      continue;
+    }
+    cv::Point3f pm(cboard.get_3d_position(i));
+    cv::Point2f ps(p.x, p.y);
+    model.push_back(pm);
+    scene.push_back(ps);
+  }
+
+  cv::Mat kmat(kvec);
+  cv::Mat dmat = cv::Mat::zeros(5, 1, cv::DataType<float>::type); // No distortion
+  cv::Mat rmat(3, 1, cv::DataType<double>::type);
+  cv::Mat tmat(3, 1, cv::DataType<double>::type);
+  cv::OutputArray oRmat(rmat), oTmat(tmat);
+  cv::solvePnP(model, scene, kmat.reshape(1, 3), dmat, oRmat, oTmat);
+  float rx = rmat.at<double>(0, 0);
+  float ry = rmat.at<double>(1, 0);
+  float rz = rmat.at<double>(2, 0);
+  float rw = sqrt(rx * rx + ry * ry + rz * rz);
+  geometry_msgs::Pose pose;
+  pose.position.x = tmat.at<double>(0, 0);
+  pose.position.y = tmat.at<double>(1, 0);
+  pose.position.z = tmat.at<double>(2, 0);
+  pose.orientation.x = rw > 0 ? sin(rw / 2) * rx / rw : rx;
+  pose.orientation.y = rw > 0 ? sin(rw / 2) * ry / rw : ry;
+  pose.orientation.z = rw > 0 ? sin(rw / 2) * rz / rw : rz;
+  pose.orientation.w = cos(rw / 2);
+  pub2->publish(pose);
+  return true;
 }
 
 bool reload(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
@@ -120,13 +90,6 @@ bool reload(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
     }
   }
   cboard.init();
-  double umm;
-  if (! nh->getParam("gridboard/unitmm", umm))
-  {
-    ROS_ERROR("GetGrid::paramer \"unitmm\" not found");
-    return false;
-  }
-  px2mm = umm / cboard.para["unitleng"];
   if (! nh->getParam(paramK.c_str(), kvec))
   {
     ROS_ERROR("GetGrid::paramer \"K\" not found");
@@ -152,6 +115,10 @@ int main(int argc, char **argv)
   std_srvs::Trigger::Request req;
   std_srvs::Trigger::Response res;
   reload(req, res);
+  ros::Publisher p1 = n.advertise<sensor_msgs::Image>("gridboard/image", 1);
+  pub1 = &p1;
+  ros::Publisher p2 = n.advertise<geometry_msgs::Pose>("gridboard/pose", 1);
+  pub2 = &p2;
   ros::spin();
   return 0;
 }
