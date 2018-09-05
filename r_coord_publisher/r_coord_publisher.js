e95ce6973eed96a0035461dc46b98913b5772bf6
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-09-05 19:57:18 +0900

    r_coord...tested

--- r_coord_publisher/r_coord_publisher.js
+++ r_coord_publisher/r_coord_publisher.js
@@ -8,23 +8,18 @@ const net = require('net');
 const ns = '/robot';
 
 function toTfEuler(data) {
-  // data format is '(X,Y,Z,A,B,C)(F1,F2)\n'.
-  // We use X, Y, Z, A, B, and C.
-  const xyzabc = data.toString().split(',', 6);
-
-  if (xyzabc.length !== 6) {
-    return;
+// data format is 'P1******(X,Y,Z,A,B,C)(F1,F2)(J1,J2,J3,J4,J5,J6)\n'.
+  const str=data.toString();
+  const ary=str.replace(/\)\(/g,'],[').replace(/P1.*\(/,'[').replace(/\)/,']').replace(/\+/g,'');
+  console.log(ary);
+  let coords=[];
+  try{
+    coords=JSON.parse('['+ary+']');
   }
-  else {
-    let tfe = new geometry_msgs.Transform();
-    tfe.translation.x = parseFloat(xyzabc[0].slice(1)); // discard '('
-    tfe.translation.y = parseFloat(xyzabc[1]);
-    tfe.translation.z = parseFloat(xyzabc[2]);
-    tfe.rotation.x = parseFloat(xyzabc[3]);
-    tfe.rotation.y = parseFloat(xyzabc[4]);
-    tfe.rotation.z = parseFloat(xyzabc[5]); // parseFloat() discards ')(F1')
-    return tfe;
+  catch(e){
+    console.log("error "+e);
   }
+  return coords;
 }
 
 setImmediate(async function() {
@@ -38,16 +33,35 @@ setImmediate(async function() {
   const server = net.createServer(function(conn) {
     console.log('connection established');
     conn.on('data', function(data) {
-      //console.log('got data[' + data + ']');
-      const tf_euler = toTfEuler(data);
-      if (tf_euler) {
-        //console.log('publish... tf_euler.translation.x=' + tf_euler.translation.x);
-        pub_euler.publish(tf_euler);
+      const coords = toTfEuler(data);
+      if(coords.length<3){
+        console.log('r_coord error:'+coords);
+        return;
+      }
+      if(coords[0].length==6){
+        let tfe = new geometry_msgs.Transform();
+        tfe.translation.x = coords[0][0];
+        tfe.translation.y = coords[0][1];
+        tfe.translation.z = coords[0][2];
+        tfe.rotation.x = coords[0][3];
+        tfe.rotation.y = coords[0][4];
+        tfe.rotation.z = coords[0][5];
+        pub_euler.publish(tfe);
+      }
+      if(coords[2].length==6){
+        let jnt = new sensor_msgs.JointState();
+        jnt.name='RV4FL';
+        coords[2].forEach(function(c){
+          jnt.position.push(c);
+          jnt.velocity.push(0);
+          jnt.effort.push(0);
+        });
+//        pub_js.publish(jnt);
       }
     });
     conn.on('close', function() {
       console.log('connection closed');
     });
-  }).listen(7373);
+  }).listen(10002);
 
 });
