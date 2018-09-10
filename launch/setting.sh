d7997212196636a7473773ff7b849f2419b072d4
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-09-10 17:05:48 +0900

    Add delay to right camera

--- launch/setting.sh
+++ launch/setting.sh
@@ -2,5 +2,5 @@
 
 echo "Camera setting"
 arv-tool-0.4 control GevSCPSPacketSize=9000
-arv-tool-0.4 control TriggerDelay=50
+arv-tool-0.4 control -n SENTECH-17C8087 TriggerDelay=50
 echo "Network setting"
