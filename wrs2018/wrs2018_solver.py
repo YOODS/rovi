9babd7aea55e11457824387342a4a0d4efefc85c
take-w <takebayashi@yoods.co.jp>
2018-09-27 19:56:49 +0900

    HALCON認識実装はしたけれど
    yodpy.soはyodpy/source/src-lib-python-viewから作ったもの。
    (何も見つからなかった時のバグ修正入り。)
    
    ゆるくしても何も見つからない。。
    
    たまに点群ply (/tmp/test.ply) が小さい?遠い?感じになることがあるようで、
    それも対応が必要そうではある。
    (しかし何も見つからないのはそれが原因ではなさそう。)

--- wrs2018/wrs2018_solver.py
+++ wrs2018/wrs2018_solver.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import roslib
 import rospy
-import yodpy2
+import yodpy
 from rovi.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from std_msgs.msg import Bool
@@ -19,30 +19,8 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../script'))
 import tflib
 
-zLimit=100.0 #to eliminate far points
 Tolerance=0.001
 Rejection=2.5
-Mesh=1
-Area=np.array([[1.0e6,-1.0e6],[1.0e6,-1.0e6],[1.0e6,-1.0e6]])
-
-def voxel(D):
-  global Area
-  chgf=False
-  for axis,vox in zip(D.T,Area):
-    p1=np.min(axis)
-    if vox[0]>p1:
-      vox[0]=p1
-      chgf=True
-    p2=np.max(axis)
-    if vox[1]<p2:
-      vox[1]=p2
-      chgf=True
-  area=tuple(map(tuple,Area))
-  if chgf is True:
-    print "Remeshing",area
-    yodpy2.makeMesh(area=area,mesh=Mesh)
-  ret,pc = yodpy2.normalize(np.ravel(D))
-  return pc.reshape((-1,3))
 
 def P0():
   return np.array([]).reshape((-1,3))
@@ -54,7 +32,7 @@ def np2F(d):  #numpy to Floats
 
 def np2PC(d):  #numpy to PointCloud
   pc=PointCloud()
-  pc.header.frame_id='hand'
+  pc.header.frame_id='hand' # TODO?
   f=np.ravel(d)
   for e in f.reshape((-1,3)):
     p=Point32()
@@ -64,51 +42,74 @@ def np2PC(d):  #numpy to PointCloud
     pc.points.append(p)
   return pc
 
-def cb_load(str):
-  global modPn,modMk
-  if len(str.data)==0:
-    modPn=P0()
-    modMk=P0()
-    pub_mdf.publish(np2F(modPn))
-    pub_mdk.publish(np2PC(modMk))
-  else:
-    P=cv2.ppf_match_3d.loadPLYSimple(str.data+'.ply',0)
-    print 'load PC',P.dtype,P.shape
-    modPn=np.vstack((modPn,P))
-    pub_mdf.publish(np2F(modPn))
-    P=cv2.ppf_match_3d.loadPLYSimple(str.data+'_m.ply',0)
-    print 'load Marker',P.dtype,P.shape
-    modMk=np.vstack((modMk,P))
-    pub_mdk.publish(np2PC(modMk))
-  return
-
-def cb_save(str):
-  global scnPn,scnMk,modPn,modMk
-  modPn=np.copy(scnPn)
-  modMk=np.copy(scnMk)
-  scnPn=P0()
-  scnMk=P0()
-  pub_scf.publish(np2F(scnPn))
-  pub_mdf.publish(np2F(modPn))
-  pub_sck.publish(np2PC(scnMk))
-  pub_mdk.publish(np2PC(modMk))
-  if len(str.data)>0:
-    d=modPn.astype(np.float32)
-    print 'save model PC',d.dtype,d.shape
-    P=cv2.ppf_match_3d.writePLY(d,str.data+'.ply')
-    d=modMk.astype(np.float32)
-    print 'save model Marker',d.dtype,d.shape
-    P=cv2.ppf_match_3d.writePLY(d,str.data+'_m.ply')
+def prepare_model(stl_file):
+  global model, is_prepared
+  result = yodpy.loadSTL(stl_file)
+  retcode = result[0]
+  model = result[1]
+  print('loadSTL retcode=',retcode)
+  if retcode == 0:
+    retcode = yodpy.train3D(model)
+    print('train3D retcode=',retcode)
+    if retcode == 0:
+      is_prepared = True
   return
 
 def cb_ps(msg): #callback of ps_floats
+  #global model
+
+  print "cb_ps called!"
+
+  if not is_prepared:
+    print "ERROR: prepare_model() is NOT done. ignore this ps_floats."
+    return
+
+  result = yodpy.loadPLY("/tmp/test.ply")
+  retcode = result[0]
+  scene = result[1]
+  print('loadPLY retcode=',retcode)
+  print('loadPLY scene=',scene)
+
+  if retcode != 0:
+    print "ERROR: loadPLY() failed. ignore this ps_floats."
+    return
+
+  #result = yodpy.match3D(scene,0.11)
+  result = yodpy.match3D(scene,0.005)
+  retcode = result[0]
+  transforms = result[1]
+  matchRates = result[2]
+  print('match3D retcode=',retcode)
+  print('match3D transforms size=',len(transforms))
+  print('match3D matchRates size=',len(matchRates))
+
+  print('match3D transforms type=',type(transforms))
+  print('match3D matchRates type=',type(matchRates))
+
+  for matchRate in matchRates:
+    print('match3D matchRate type=',type(matchRate))
+    print('match3D matchRate=',matchRate)
+
+  return
+
+"""
+
+# normalize transform
+if retcode == 0:
+	for transform in transforms:
+		result = yodpy.normalize(transform,0.001)
+		retcode = result[0]
+		pc = result[1]
+		print('normalize retcode=',retcode)
+		print('normalize pc=',pc)
+"""
+
+"""
   global bTmLat,cPoLat,bTc,scnPn,scnMk
   mTb=np.linalg.inv(bTmLat)
   P=np.reshape(msg.data,(-1,3))
   n,m=P.shape
   print "PointClour In:",n
-#  P=P[np.where(P.T[2]<zLimit)]
-  P=voxel(P)
   print "PC(camera)",P
   n,m=P.shape
   print "PointClour Out:",n
@@ -123,7 +124,7 @@ def cb_ps(msg): #callback of ps_floats
   scnMk=np.vstack((scnMk,P))
   pc=np2PC(scnMk)
   pub_sck.publish(pc)
-  return
+"""
 
 def cb_X0(f):
   global scnPn,scnMk
@@ -150,7 +151,7 @@ def cb_X1(f):
   return
 
 def cb_X2(f):
-  global scnPn,scnMk,modPn,modMk
+  global scnPn,scnMk,modPn
   print "X2:ICP",modPn.shape,scnPn.shape
   icp=cv2.ppf_match_3d_ICP(100,Tolerance,Rejection,8)
   mp=modPn.astype(np.float32)
@@ -169,7 +170,6 @@ def cb_X3(f):
   pub_scf.publish(np2F(scnPn))
   pub_mdf.publish(np2F(modPn))
   pub_sck.publish(np2PC(scnMk))
-  pub_mdk.publish(np2PC(modMk))
   return
 
 def cb_pos(p):
@@ -182,16 +182,10 @@ def cb_tf(tf):
   bTm=tflib.toRT(tf)
   return
 
-def cb_parse(str):
-  exec("global zLimit,Tolerance,Rejection\n"+str.data)
-  return
-
 ########################################################
 
 rospy.init_node("solver",anonymous=True)
 ###Input topics
-rospy.Subscriber("/solver/load",String,cb_load)  #load ply
-rospy.Subscriber("/solver/save",String,cb_save)  #save ply
 rospy.Subscriber("/robot/tf",Transform,cb_tf)
 rospy.Subscriber("/detector/position2",Point,cb_pos)
 rospy.Subscriber("/rovi/ps_floats",numpy_msg(Floats),cb_ps)
@@ -199,7 +193,6 @@ rospy.Subscriber("/solver/X0",Bool,cb_X0)  #Clear scene
 rospy.Subscriber("/solver/X1",Bool,cb_X1)  #Capture position of marker and robot
 rospy.Subscriber("/solver/X2",Bool,cb_X2)  #Calc transform
 rospy.Subscriber("/solver/X3",Bool,cb_X3)  #redraw
-rospy.Subscriber('/solver/parse',String,cb_parse)
 
 ###Output topics
 pub_tf=rospy.Publisher("/solver/tf",Transform,queue_size=1)
@@ -209,20 +202,23 @@ pub_mdf=rospy.Publisher("/model/floats",numpy_msg(Floats),queue_size=1)
 pub_mdk=rospy.Publisher("/model/marker",PointCloud,queue_size=1)
 
 ###Transform
+"""
 bTc=tflib.toRT(tflib.dict2tf(rospy.get_param('/robot/calib/bTc')))  #Base to Camera
 print bTc
 bTmLat=np.eye(4).astype(float)  #robot RT when captured
 cPoLat=np.array([0,0,0],dtype=float).reshape((-1,3))  #marker coordinate when captured
 bTm=np.eye(4).astype(float) 
 cPo=np.array([0,0,0],dtype=float).reshape((-1,3))
+"""
 
 ###Globals
 scnPn=P0()  #Scene points
 scnMk=P0()  #Scene Marker points
 modPn=P0()  #Model points
-modMk=P0()  #Model Marker points
+is_prepared = False
 
 try:
+  prepare_model(os.environ['ROVI_PATH'] + '/wrs2018/model/Gear.stl')
   rospy.spin()
 except KeyboardInterrupt:
   print "Shutting down"
