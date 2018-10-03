d0f60d642f5e51a4cb627d528fc4b50f84467414
take-w <takebayashi@yoods.co.jp>
2018-10-03 20:18:49 +0900

    ベース座標に点群出すところまで (ひとまず見やすいようにオレンジ色で)
    カメラ撮影点群はmeter単位だが、
    既存処理がうまく動く用に、それ以降の処理はすべてmm単位にした。
    最後にRViZで出すためのnp2Fm()で、meterにして吐いている。

--- wrs2018/wrs2018_solver.py
+++ wrs2018/wrs2018_solver.py
@@ -26,42 +26,10 @@ Rejection=2.5
 def P0():
   return np.array([]).reshape((-1,3))
 
-def np2F(d):  #numpy to Floats
+def np2Fm(d):  #numpy to Floats (unit is meter for RViZ)
   f=Floats()
   f.data=np.ravel(d) / 1000.0
   return f
-
-def np2PCm(d):  #numpy to PointCloud for RViz. frame_id is base_link and unit is meter.
-  pc = PointCloud()
-  pc.header.stamp = rospy.Time.now(); # TODO ?
-  pc.header.frame_id = 'disp' # TODO ?
-  ch_r = ChannelFloat32()
-  ch_r.name = "r"
-  ch_g = ChannelFloat32()
-  ch_g.name = "g"
-  ch_b = ChannelFloat32()
-  ch_b.name = "b"
-  pc.channels.append(ch_r)
-  pc.channels.append(ch_g)
-  pc.channels.append(ch_b)
-  f = np.ravel(d)
-  for e in f.reshape((-1, 3)):
-    p = Point32()
-    p.x = e[0] / 1000.0
-    p.y = e[1] / 1000.0
-    p.z = e[2] / 1000.0
-    pc.points.append(p)
-    ch_r.values.append(200 / 255.0) # TODO
-    ch_g.values.append(0 / 255.0) # TODO
-    ch_b.values.append(0 / 255.0) # TODO
-    #print 'x=', p.x
-    #print 'y=', p.y
-    #print 'z=', p.z
-
-  tmpd = d.astype(np.float32)
-  cv2.ppf_match_3d.writePLY(tmpd, '/tmp/mytest.ply')
-
-  return pc
 
 def prepare_model(stl_file):
   global model, is_prepared
@@ -135,12 +103,14 @@ def cb_ps(msg): #callback of ps_floats
   n,m=P.shape
   #P=voxel(P)
   print "PointCloud In:",n
-  print "PC(camera)",P
+  #print "PC(camera in meter)",P
+  P=P*1000
+  #print "PC(camera in mm)",P
   n,m=P.shape
-  print "PointCloud Out:",n
+  #print "PointCloud Out:",n
   P=np.vstack((P.T,np.ones((1,n))))
   P=np.dot(bTm[:3],np.dot(mTc,P)).T
-  print "P2",P # now unit is mm
+  #print "P2",P
   print P.shape
   if (not np.isnan(xmin)):
     W = np.where(P.T[0] >= xmin)
@@ -160,22 +130,20 @@ def cb_ps(msg): #callback of ps_floats
   if (not np.isnan(zmax)):
     W = np.where(P.T[2] <= zmax)
     P=P[W[len(W)-1]]
-  print "where",P
+  #print "where",P
   n,m=P.shape
   print P.shape
-  print "PC",P
+  #print "PC",P
   P=P.reshape((-1,3))
   scnPn=np.vstack((scnPn,P))
-  pub_scf.publish(np2F(scnPn))
-  #pub_scp.publish(np2PCm(scnPn))
+  pub_scf.publish(np2Fm(scnPn))
   return
 
 def cb_X0(f):
   global scnPn
   print "X0:scene reset"
   scnPn=P0()
-  #pub_scf.publish(np2F(scnPn))
-  #pub_scp.publish(np2PCm(scnPn))
+  pub_scf.publish(np2Fm(scnPn))
   return
 
 def cb_X1(f):
@@ -205,7 +173,7 @@ def cb_X2(f):
   TR=np.linalg.inv(RT)
   P=np.vstack((scnPn.T,np.ones((1,len(scnPn)))))
   scnPn=np.dot(TR[:3],P).T
-  pub_scf.publish(np2F(scnPn))
+  pub_scf.publish(np2Fm(scnPn))
 
   #sprintf_s(buf, sizeof(buf), "OK\x0d(%.3f,%.3f,%.3f,%.3f,%.3f,%.3f)(7,0)\x0d", rpos.x, rpos.y, rpos.z, rpos.rx, rpos.ry, rpos.rz);
   #sprintf_s(buf, sizeof(buf), "NG\x0d");
@@ -231,13 +199,12 @@ rospy.Subscriber("/solver/X2",Bool,cb_X2)  #Recognize work and calc picking pose
 ###Output topics
 pub_tf=rospy.Publisher("/solver/tf",Transform,queue_size=1)
 pub_scf=rospy.Publisher("/scene/floats",numpy_msg(Floats),queue_size=1)
-#pub_scp=rospy.Publisher("/scene/pc",PointCloud,queue_size=1)
 pub_Y1=rospy.Publisher('/solver/Y1',Bool,queue_size=1)    #X1 done
 pub_Y2=rospy.Publisher('/solver/Y2',Bool,queue_size=1)    #X2 done
 
 ###Transform
 mTc=tflib.toRT(tflib.dict2tf(rospy.get_param('/robot/calib/mTc')))  # arM tip To Camera
-print "mTc=", mTc
+#print "mTc=", mTc
 bTmLat=np.eye(4).astype(float)  #robot RT when captured
 bTm=np.eye(4).astype(float) 
 
