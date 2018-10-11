a13576a65a505191448231c1c607110ec8747267
NaokoSAKKA <naoko@yoods.co.jp>
2018-10-11 10:05:39 +0900

    settings for onhand YCAM3D-II

--- launch/setting_17C8097.sh
+++ launch/setting_17C8097.sh
@@ -2,5 +2,5 @@
 
 echo "Camera setting"
 arv-tool-0.4 control GevSCPSPacketSize=9000
-arv-tool-0.4 control -n SENTECH-17C8102 TriggerDelay=50
+arv-tool-0.4 control -n SENTECH-17C8097 TriggerDelay=50
 echo "Network setting"
