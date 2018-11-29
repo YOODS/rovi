2345ed5bf3db02e82b2dfceac5907597269df535
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2018-11-29 17:52:28 +0900

    External trigger live mode

--- script/ycamctl.js
+++ script/ycamctl.js
@@ -20,13 +20,8 @@ const jsyaml = require('js-yaml');
 const ImageSwitcher = require('./image_switcher.js');
 const Notifier = require('./notifier.js');
 
-function sleep(n){
-  return new Promise(function(resolve){
-    setTimeout(function(){
-      resolve(true);
-    },n);
-  });
-}
+function sleep(msec){return new Promise(function(resolve){setTimeout(function(){resolve(true);},msec);});}
+
 
 setImmediate(async function() {
   const rosNode = await ros.initNode(NSycamctrl);
@@ -39,7 +34,6 @@ setImmediate(async function() {
     ros.log.error('genpc service not available');
     return;
   }
-
   let param={
     camps: new Notifier(rosNode,NSpsgenpc + '/camera'),//Genpc mode camera params
     camlv: new Notifier(rosNode,NSlive + '/camera'),  //Live mode camera params
@@ -55,6 +49,20 @@ setImmediate(async function() {
     obj[key]=val;
     await sens.pset(obj);
   });
+
+  sens.scanID=null;
+  sens.scanOn=function(){
+    if(sens.scanID!=null) return;
+    param.proj.push({Go:-1});
+    sens.scanID=setTimeout(function(){
+      sens.scanID=null;
+      sens.scanOn();
+    },Math.floor(1000/param.camlv.objs.AcquisitionFrameRate));
+  }
+  sens.scanOff=function(){
+    if(sens.scanID!=null) clearTimeout(sens.scanID);
+    sens.scanID=null;
+  }
 
   let sensEv;
   switch (sensName) {
@@ -79,12 +87,15 @@ setImmediate(async function() {
   });
   sensEv.on('wake', async function() {
     console.log('ycam wake');
-    for(let n in param) param[n].start();
-    await sens.cset({ 'TriggerMode': 'Off' });
+    for(let n in param) await param[n].start();
+    param.camlv.push({TriggerMode:'On'});
+    param.proj.push({Mode:1});//--- let projector pattern to be phase shift
+    setTimeout(sens.scanOn,3000);
     ros.log.warn('NOW ALL READY');
   });
   sensEv.on('shutdown', async function() {
     console.log('ycam down');
+    sens.scanOff();
     for(let n in param) param[n].reset();
   });
   sensEv.on('left', async function(img) {
@@ -97,34 +108,37 @@ setImmediate(async function() {
 
 
 // ---------Definition of services
-  let failcnt=0;
+  let pserror=0,psthres=0;
   let psgenpc = function(req,res){
+    if(pserror<0) return false;
     if (!sens.normal) {
       ros.log.warn(res.message = 'YCAM not ready');
       res.success = false;
       return true;
     }
     return new Promise(async (resolve) => {
-      ros.log.info('Failed '+failcnt+' times');
-      const timeoutmsec = param.proj.objs.Interval*13*2 + 1000;
-      let wdt = setTimeout(async function() { // <--------watch dog
+      let wdt = setTimeout(async function() { //---start watch dog
         image_L.cancel();
         image_R.cancel();
-        await sens.cset({ 'TriggerMode': 'Off' });
+        sens.pblock=false;//---release pset method
+        sens.scanOn();
         const errmsg = 'pshift_genpc timed out AAA';
         ros.log.error(errmsg);
         res.success = false;
         res.message = errmsg;
-        failcnt++;
+        pserror++;
         resolve(true);
-      }, timeoutmsec);
-      await sens.cset({ 'TriggerMode': 'On' });
-      await sens.cset(param.camps.objs);
+      }, param.proj.objs.Interval*13*2 + 1000);
+
+      sens.scanOff();
+//      sens.pblock=true;//---block pset method
+      await sens.cset(param.camps.objs); //---overwrites genpc camera params
       await sleep(10);
       setTimeout(function(){
-    	  sens.pset({ 'Go': 2 }); // <--------projector sequence start
+        sens.pblock=false;//---release pset method
+        sens.pset({ 'Go': 2 }); //---projector starts after 100ms
       },100);
-      let imgs=await Promise.all([image_L.store(13,50),image_R.store(13,50)]);
+      let imgs=await Promise.all([image_L.store(13,100),image_R.store(13,100)]);//---after 100msec, try to store 13 images
       clearTimeout(wdt);
       let gpreq = new rovi_srvs.GenPC.Request();
       gpreq.imgL = imgs[0];
@@ -140,15 +154,14 @@ setImmediate(async function() {
         res.success = false;
       }
       let ts=Math.floor(gpres.pc_cnt*0.001)+1;
-      setTimeout(function(){
-        sens.cset({ 'TriggerMode': 'Off' });
-      },ts);
-      await sens.cset(param.camlv.diff(param.camps.objs));
+      setTimeout(function(){ sens.scanOn();},ts);//---after "ts" msec, back to live mode
+      param.camlv.push(param.camlv.diff(param.camps.objs));//---restore overwritten camera params
       image_L.view(vue_N);
       image_R.view(vue_N);
-      image_L.cancel(ts);
+      image_L.cancel(ts);//---after "ts" msec, back to thru mode
       image_R.cancel(ts);
-      ros.log.info('Time to publish pc '+ts+' ms');
+      if(gpres.pc_cnt<psthres) pserror=-101;
+      ros.log.info('Time to publish pc '+ts+' ms, errors:'+pserror);
       resolve(true);
     });
   }
@@ -190,7 +203,7 @@ setImmediate(async function() {
         res.answer = await sens.pset(obj);
       }
       return Promise.resolve(true);
-    case 'stat': // <--------sensor (maybe YCAM) status query
+    case 'stat': //---sensor(maybe YCAM) status query
       return new Promise((resolve) => {
         res.answer = JSON.stringify(sens.stat());
         resolve(true);
@@ -198,7 +211,7 @@ setImmediate(async function() {
     case 'view':
       return new Promise((resolve) => {
         try {
-          vue_N = parseInt(cmds[0]);
+          vue_N = Number(cmds[0]);
         }
         catch(err) {
           vue_N = 0;
@@ -207,6 +220,16 @@ setImmediate(async function() {
         image_R.view(vue_N);
         resolve(true);
       });
+    case 'thres':
+      pserror=0;
+      psthres=Number(cmds[0]);
+      return Promise.resolve(true);
+    case 'cstop':
+      sens.scanOff();
+      return Promise.resolve(true);
+    case 'cstart':
+      sens.scanOn();
+      return Promise.resolve(true);
     }
   });
 });
