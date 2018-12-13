4a101fe7a047fb9e7d7f67ef178c76af7a922b43
take-w <takebayashi@yoods.co.jp>
2018-12-13 12:14:17 +0900

    いったんyamlをまとめなおした。既存の使い方ができるように。rqt_param_managerはこの状態では使えない。

--- launch/ycam3sxga.sh
+++ launch/ycam3sxga.sh
@@ -8,8 +8,8 @@ export ROS_MASTER_URI=http://localhost:11311
 export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
 
 roscd rovi
-ROS_NAMESPACE=/rovi rosparam load yaml/ycam3sxga_livecamera_ld.yaml
-ROS_NAMESPACE=/rovi rosparam load yaml/ycam3sxga_pshift_ld.yaml
+#ROS_NAMESPACE=/rovi rosparam load yaml/ycam3sxga_livecamera_ld.yaml
+#ROS_NAMESPACE=/rovi rosparam load yaml/ycam3sxga_pshift_ld.yaml
 ROS_NAMESPACE=/rovi rosparam load yaml/ycam3sxga.yaml
 script/gvloadSXGA.js
 roslaunch launch/ycam3s.launch
--- launch/ycam3vga.sh
+++ launch/ycam3vga.sh
@@ -8,8 +8,8 @@ export ROS_MASTER_URI=http://localhost:11311
 export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
 
 roscd rovi
-ROS_NAMESPACE=/rovi rosparam load yaml/ycam3vga_livecamera_ld.yaml
-ROS_NAMESPACE=/rovi rosparam load yaml/ycam3vga_pshift_ld.yaml
+#ROS_NAMESPACE=/rovi rosparam load yaml/ycam3vga_livecamera_ld.yaml
+#ROS_NAMESPACE=/rovi rosparam load yaml/ycam3vga_pshift_ld.yaml
 ROS_NAMESPACE=/rovi rosparam load yaml/ycam3vga.yaml
 script/gvloadVGA.js
 roslaunch launch/ycam3s.launch
