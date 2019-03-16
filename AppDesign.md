be2a6e2707afb700437a8c85c190088628ad88fd
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-03-16 23:08:31 +0900

    notification string changed

--- AppDesign.md
+++ AppDesign.md
@@ -11,11 +11,10 @@
 |/solver/X0|std_msgs/Bool|Clear captured data|
 |/solver/X1|std_msgs/Bool|Capture|
 |/solver/X2|std_msgs/Bool|Solve|
-|/solver/X3|std_msgs/Bool||
-|/solver/Y0|std_msgs/Bool|Response for /solver/X0|
-|/solver/Y1|std_msgs/Bool|Response for /solver/X1|
-|/solver/Y2|std_msgs/Bool|Response for /solver/X2|
-|/solver/Y2|std_msgs/Bool||
+|/solver/X3|std_msgs/Bool|Load...|
+|/solver/X4|std_msgs/Bool|Save...|
+|/solver/Y[0-9]|std_msgs/Bool|Response for /solver/X[0-9]|
+|/solver/message|std_msgs/String|Message|
 |/solver/error|std_msgs/String|Error code and message|
 |/solver/tf|geometry_msgs/Transform|Rigid transform solved|
 |/robot/tf|geometry_msgs/Transform|Current transform of robot(base=0,tool=0)|
--- script/param_watch.js
+++ script/param_watch.js
@@ -6,10 +6,10 @@ const Notifier = require('./notifier.js');
 
 setImmediate(async function() {
   if(process.argv.length<4){
-    console.log('usage: paraman.js <node of parameter> <topic to inform changing>...');
+    console.log('usage: param_watch.js <node of parameter> <topic to inform changing>...');
     process.exit(1)
   }
-  const rosNode=await ros.initNode('paraman_'+ros.Time.now().secs+'.'+ros.Time.now().nsecs.toString().substr(0,3));
+  const rosNode=await ros.initNode('param_wat_'+ros.Time.now().secs+'.'+ros.Time.now().nsecs.toString().substr(0,3));
   const notif=new Notifier(rosNode,process.argv[2])
   let pubs=[];
   for(let i=3;i<process.argv.length;i++){
@@ -21,9 +21,8 @@ setImmediate(async function() {
   }
   notif.on('change',function(key,val){
     let msg=new std_msgs.String();
-    let v=Number(val);
-    if(Number.isNaN(v)) v='"'+val+'"';
-    msg.data=key+'='+v;
+    if(Number.isNaN(Number(val))) msg.data="{"+"'"+key+"'"+":"+JSON.stringify(val)+"}";
+    else msg.data="{"+"'"+key+"'"+":"+val+"}";
     pubs.forEach(function(p){
       p.publish(msg);
     });
