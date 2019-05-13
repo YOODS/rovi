1752fbed067c07e06e2e5a56961a186d3a6273a9
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-05-13 17:10:13 +0900

    QT..PATH added

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: install
+
+install:
+	sudo cp launch/rovirun.sh /usr/local/bin
+	cp script/tflib.py ../../devel/lib/python2.7/dist-packages/
+
--- launch/rovirun.sh
+++ launch/rovirun.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 export NODE_PATH=/usr/lib/node_modules
+export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
 source /opt/ros/kinetic/setup.bash
-cd ~/
-cd */src/rovi
-cd ../../
-source devel/setup.bash
+cd ~/*/src/rovi
+cd ../../devel
+source setup.bash
 export ROS_HOSTNAME=localhost
 export ROS_MASTER_URI=http://localhost:11311
-export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
+export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/qt5/plugins/platforms
 
 $1 $2 $3 $4 $5 $6 $7 $8
+
+sleep 3 
+
