53defa370c1381d27bd3831c5ef880ef7343781f
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-05-17 12:20:20 +0900

    unit conversion(m) added

--- launch/ycam3loader.sh
+++ launch/ycam3loader.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 source /opt/ros/kinetic/setup.bash
-source ~/catkin_ws/devel/setup.bash
+cd ~/*/src/rovi
+cd ../../
+source devel/setup.bash
 
 export ROS_NAMESPACE=/rovi
 
@@ -10,6 +12,12 @@ if [ "$res" = "" ]
 then
   res=sxga
 fi
+unit=$2
+if [ "$unit" = "" ]
+then
+  unit=m
+fi
+
 roscd rovi
 rosparam load yaml/ycam3$res.yaml
 IPADDS=$(rosparam get camera/address)
@@ -21,12 +29,14 @@ do
   then
     break
   fi
+  sleep 2
   echo 'ycam3loader::No device'
 done
 
 echo "[ycam3loader]" $guid $IPADDS
 rosparam set camera/guid "$guid"
 script/gvload.js $res "$guid"
+script/p2qmatrix.py $unit
 
 roslaunch launch/ycam3s.launch
 pkill camnode
--- script/p2qmatrix.py
+++ script/p2qmatrix.py
@@ -3,15 +3,32 @@
 import numpy as np
 import roslib
 import rospy
+import sys
+
+try:
+  unit=sys.argv[1]
+except Exception as e:
+	unit="m"
 
 rospy.init_node('p2qmatrix',anonymous=True)
 
 P1=rospy.get_param('/rovi/left/remap/P')
 P2=rospy.get_param('/rovi/right/remap/P')
+if unit is "m":
+  if P2[3]>1:
+    P2[3]=P2[3]*0.001
+    P2[7]=P2[7]*0.001
+    P2[11]=P2[11]*0.001
+else:
+  if P2[3]<1:
+    P2[3]=P2[3]*1000
+    P2[7]=P2[7]*1000
+    P2[11]=P2[11]*1000
+
 Q=np.zeros(16)
 Q[0]=Q[5]=1
 Q[3]=-P1[2] #-cx1
-Q[7]=-P1[2] #-cy1
+Q[7]=-P1[6] #-cy1
 Q[11]=P1[0] #f
 Tx=P2[3]/P1[0]
 Q[14]=-1/Tx
